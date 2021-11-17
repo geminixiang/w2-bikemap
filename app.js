7a540701c6f8ba63a8415f69acc81c4bbe7cd3cb
f416720001-win11 <f416720001@yahoo.com.tw>
2021-11-17 17:48:02 +0800

    更改美食來源、添加美食/景點marker

--- app.js
+++ app.js
@@ -28,6 +28,22 @@ var grayIcon = new L.Icon({
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
 });
+var foodIcon = new L.Icon({
+  iconUrl: "./icons/foodmarker.svg",
+  shadowUrl: "",
+  iconSize: [62, 75],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+var tourIcon = new L.Icon({
+  iconUrl: "./icons/tour.svg",
+  shadowUrl: "",
+  iconSize: [62, 75],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 function success(pos) {
   var crd = pos.coords;
@@ -75,35 +91,35 @@ function readTextFile(file, callback) {
 
 // 想佔存車站資料，尚未使用
 // https://stackoverflow.com/questions/34951170/save-json-to-chrome-storage-local-storage
-var local = (function () {
-  var setData = function (key, obj) {
-    var values = JSON.stringify(obj);
-    localStorage.setItem(key, values);
-  };
+// var local = (function () {
+//   var setData = function (key, obj) {
+//     var values = JSON.stringify(obj);
+//     localStorage.setItem(key, values);
+//   };
 
-  var getData = function (key) {
-    if (localStorage.getItem(key) != null) {
-      return JSON.parse(localStorage.getItem(key));
-    } else {
-      return false;
-    }
-  };
+//   var getData = function (key) {
+//     if (localStorage.getItem(key) != null) {
+//       return JSON.parse(localStorage.getItem(key));
+//     } else {
+//       return false;
+//     }
+//   };
 
-  var updateDate = function (key, newData) {
-    if (localStorage.getItem(key) != null) {
-      var oldData = JSON.parse(localStorage.getItem(key));
-      for (keyObj in newData) {
-        oldData[keyObj] = newData[keyObj];
-      }
-      var values = JSON.stringify(oldData);
-      localStorage.setItem(key, values);
-    } else {
-      return false;
-    }
-  };
+//   var updateDate = function (key, newData) {
+//     if (localStorage.getItem(key) != null) {
+//       var oldData = JSON.parse(localStorage.getItem(key));
+//       for (keyObj in newData) {
+//         oldData[keyObj] = newData[keyObj];
+//       }
+//       var values = JSON.stringify(oldData);
+//       localStorage.setItem(key, values);
+//     } else {
+//       return false;
+//     }
+//   };
 
-  return { set: setData, get: getData, update: updateDate };
-})();
+//   return { set: setData, get: getData, update: updateDate };
+// })();
 
 // 搜尋API設定
 var searchOptions = {
@@ -219,25 +235,29 @@ function renderSidebar() {
   getTourismData();
 }
 
-function setMarker() {
-  filterData.forEach((item) => {
-    var StationName = item.StationName.Zh_tw.replace("YouBike", "")
-      .replace("2.0_", "")
-      .replace("1.0_", "");
+function setMarker(data, type) {
+  if (type == "bike") {
+    console.log("自行車marker渲染");
+    data.forEach((item) => {
+      var StationName = item.StationName.Zh_tw.replace("YouBike", "")
+        .replace("2.0_", "")
+        .replace("1.0_", "");
 
-    if (item.AvailableRentBikes > 0) {
-      mask = greenIcon;
-      markers.addLayer(
-        L.marker(
-          [item.StationPosition.PositionLat, item.StationPosition.PositionLon],
-          {
-            icon: mask,
-            title: StationName,
-            alt: "dlksafj;lksadjflk;"
-          }
-        )
-          .bindPopup(
-            `<div id="card">
+      if (item.AvailableRentBikes > 0) {
+        mask = greenIcon;
+        markers.addLayer(
+          L.marker(
+            [
+              item.StationPosition.PositionLat,
+              item.StationPosition.PositionLon
+            ],
+            {
+              icon: mask,
+              title: StationName
+            }
+          )
+            .bindPopup(
+              `<div id="card">
                 <div class="card-body">
                 <h1 class="card-title" id="StationName">${StationName}</h1>
                 <h6 class="card-subtitle mb-2 text-muted" id="StationAddress">${item.StationAddress.Zh_tw}</h6>
@@ -246,28 +266,31 @@ function setMarker() {
                 <p class="card-text mt-0">更新時間：<span id="UpdateTime">${item.UpdateTime}</span></p>
                 </div>
             </div>`,
+              {
+                closeButton: false
+              }
+            )
+            .bindTooltip(StationName, {
+              permanent: true,
+              direction: "center",
+              className: "mytooltip"
+            })
+        );
+      } else {
+        mask = grayIcon;
+        markers.addLayer(
+          L.marker(
+            [
+              item.StationPosition.PositionLat,
+              item.StationPosition.PositionLon
+            ],
             {
-              closeButton: false
+              icon: mask,
+              title: StationName
             }
           )
-          .bindTooltip(StationName, {
-            permanent: true,
-            direction: "center",
-            className: "mytooltip"
-          })
-      );
-    } else {
-      mask = grayIcon;
-      markers.addLayer(
-        L.marker(
-          [item.StationPosition.PositionLat, item.StationPosition.PositionLon],
-          {
-            icon: mask,
-            title: StationName
-          }
-        )
-          .bindPopup(
-            `<div id="card" class="noBikePopUp">
+            .bindPopup(
+              `<div id="card" class="noBikePopUp">
               抱歉！本站已無車輛租借
                 <div class="card-body" style="display:none;">
                   <h1 class="card-title" id="StationName">${StationName}</h1>
@@ -277,39 +300,113 @@ function setMarker() {
                   <p class="card-text mt-0">更新時間：<span id="UpdateTime">${item.UpdateTime}</span></p>
                 </div>
             </div>`,
+              {
+                closeButton: false
+              }
+            )
+            .bindTooltip(StationName, {
+              permanent: true,
+              direction: "center",
+              className: "mytooltip"
+            })
+        );
+      }
+    });
+  } else if (type == "food") {
+    console.log("食物marker渲染");
+
+    data.forEach((item) => {
+      mask = foodIcon;
+      // console.log(item.lat, item.lng);
+      markers.addLayer(
+        L.marker([item.lat, item.lng], {
+          icon: mask,
+          title: item.name
+        })
+          .bindPopup(
+            `<div id="card">${item.name}
+
+              </div>`,
             {
               closeButton: false
             }
           )
-          .bindTooltip(StationName, {
+          .bindTooltip(item.name, {
             permanent: true,
             direction: "center",
             className: "mytooltip"
           })
       );
-    }
-  });
+    });
+  } else if (type == "tourism") {
+    console.log("景點marker渲染");
+
+    data.forEach((item) => {
+      mask = tourIcon;
+      console.log(item);
+      console.log(item.Position.PositionLat, item.Position.PositionLon);
+      markers.addLayer(
+        L.marker([item.Position.PositionLat, item.Position.PositionLon], {
+          icon: mask,
+          title: item.Name
+        })
+          .bindPopup(
+            `<div id="card">
+              <div class="card-body">
+                <h1 class="card-title" id="StationName">${item.Name}</h1>
+                <h6 class="card-subtitle mb-2 text-muted" id="StationAddress">${item.WebsiteUrl}</h6>
+                <p class="card-text mb-0">${item.DescriptionDetail}</p>
+              </div>
+            </div>`,
+            {
+              closeButton: false
+            }
+          )
+          .bindTooltip(item.Name, {
+            permanent: true,
+            direction: "center",
+            className: "mytooltip"
+          })
+      );
+    });
+  }
   map.addLayer(markers);
 }
 
 // 串接附近的景點資料
 let tourism = [];
 function getTourismData(longitude, latitude) {
-  axios({
-    method: "get",
-    url: "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=30&$format=JSON",
-    // url: `https://ptx.transportdata.tw/MOTC/v2/Bike/Station/NearBy?$spatialFilter=nearby(${latitude},${longitude},1000)`,
-    headers: GetAuthorizationHeader()
-  })
-    .then((response) => {
-      console.log("景點資料", response);
-      tourism = response.data;
+  if (tourism.length > 0) {
+    console.log("已有景點資料");
+    showTourismData();
+  } else {
+    console.log("取得景點API資料");
 
-      let random1 = Math.floor(Math.random() * tourism.length);
-      let random2 = Math.floor(Math.random() * tourism.length);
-      let random3 = Math.floor(Math.random() * tourism.length);
+    axios({
+      method: "get",
+      url: "https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?$top=30&$format=JSON",
+      // url: `https://ptx.transportdata.tw/MOTC/v2/Bike/Station/NearBy?$spatialFilter=nearby(${latitude},${longitude},1000)`,
+      headers: GetAuthorizationHeader()
+    })
+      .then((response) => {
+        console.log("景點資料", response);
+        tourism = response.data;
+        showTourismData();
+        setMarker(tourism, "tourism");
 
-      document.getElementById("tourism").innerHTML = `
+        // console.log(tourism);
+        // getAvailableData(longitude, latitude);
+      })
+      .catch((error) => console.log("error", error));
+  }
+}
+
+function showTourismData() {
+  let random1 = Math.floor(Math.random() * tourism.length);
+  let random2 = Math.floor(Math.random() * tourism.length);
+  let random3 = Math.floor(Math.random() * tourism.length);
+
+  document.getElementById("tourism").innerHTML = `
       <!--Pixabay 上的免費圖片-->
       <div class="food-detail">
           <img src="./images/tour1.jpg" alt="" class="food-img">
@@ -332,56 +429,65 @@ function getTourismData(longitude, latitude) {
           <p style="display: inline;">120則評論</p>
           <p>${tourism[random3].DescriptionDetail}...查看更多</p>
       </div>`;
-      // console.log(tourism);
-      // getAvailableData(longitude, latitude);
-    })
-    .catch((error) => console.log("error", error));
 }
 
 // 串接美食資料
 let foods = [];
 function getFoodData(longitude, latitude) {
-  axios({
-    method: "get",
-    // 美食API 台北資料錯誤，另改來源 https://gis.taiwan.net.tw/XMLReleaseALL_public/restaurant_C_f.json
-    // url: "https://ptx.transportdata.tw/MOTC/v2/Tourism/Restaurant/Taipei?$format=JSON",
-    // url: `https://ptx.transportdata.tw/MOTC/v2/Bike/Station/NearBy?$spatialFilter=nearby(${latitude},${longitude},1000)`,
-    url: "restaurant.json"
-    // headers: GetAuthorizationHeader()
-  })
-    .then((response) => {
-      console.log("美食資料", response.data.XML_Head.Infos.Info);
-      foods = response.data.XML_Head.Infos.Info;
-      let random1 = Math.floor(Math.random() * foods.length);
-      let random2 = Math.floor(Math.random() * foods.length);
-      let random3 = Math.floor(Math.random() * foods.length);
+  if (foods.length > 0) {
+    console.log("已有餐廳資料");
+    showFoodData();
+  } else {
+    console.log("取得餐廳API資料");
 
-      document.getElementById("restaurant").innerHTML = `
+    axios({
+      method: "get",
+      // 美食API 台北資料錯誤，另改來源 https://data.gov.tw/dataset/132491
+      // url: "https://ptx.transportdata.tw/MOTC/v2/Tourism/Restaurant/Taipei?$format=JSON",
+      // url: `https://ptx.transportdata.tw/MOTC/v2/Bike/Station/NearBy?$spatialFilter=nearby(${latitude},${longitude},1000)`,
+      url: "./data/109taipei.json"
+      // headers: GetAuthorizationHeader()
+    })
+      .then((response) => {
+        console.log(response);
+        console.log("美食資料", response.data);
+        foods = response.data;
+        showFoodData();
+        // getAvailableData(longitude, latitude);
+        setMarker(foods, "food");
+      })
+      .catch((error) => console.log("error", error));
+  }
+}
+
+function showFoodData() {
+  let random1 = Math.floor(Math.random() * foods.length);
+  let random2 = Math.floor(Math.random() * foods.length);
+  let random3 = Math.floor(Math.random() * foods.length);
+
+  document.getElementById("restaurant").innerHTML = `
       <!--Pixabay 上的免費圖片-->
       <div class="food-detail">
           <img src="./images/food1.jpg" alt="" class="food-img">
-          <h1>${foods[random1].Name}</h1>
+          <h1>${foods[random1].name}</h1>
           <p style="display: inline;">4.0</p><span>★★★★☆</span>
           <p style="display: inline;">120則評論</p>
-          <p>${foods[random1].Description}...查看更多</p>
+          <p>${foods[random1].web}...查看更多</p>
       </div>
       <div class="food-detail">
           <img src="./images/food2.jpg" alt="" class="food-img">
-          <h1>${foods[random2].Name}</h1>
+          <h1>${foods[random2].name}</h1>
           <p style="display: inline;">4.0</p><span>★★★★☆</span>
           <p style="display: inline;">120則評論</p>
-          <p>${foods[random2].Description}...查看更多</p>
+          <p>${foods[random2].web}...查看更多</p>
       </div>
       <div class="food-detail">
           <img src="./images/food3.jpg" alt="" class="food-img">
-          <h1>${foods[random3].Name}</h1>
+          <h1>${foods[random3].name}</h1>
           <p style="display: inline;">4.0</p><span>★★★★☆</span>
           <p style="display: inline;">120則評論</p>
-          <p>${foods[random3].Description}...查看更多</p>
+          <p>${foods[random3].web}...查看更多</p>
       </div>`;
-      // getAvailableData(longitude, latitude);
-    })
-    .catch((error) => console.log("error", error));
 }
 
 // 串接附近的自行車租借站位資料
@@ -427,7 +533,7 @@ function getAvailableData(longitude, latitude) {
       });
       // console.log('filterData', filterData)
 
-      setMarker();
+      setMarker(filterData, "bike");
     })
     .catch((error) => console.log("error", error));
 }
@@ -446,7 +552,20 @@ function getRoutesData() {
 
       // console.log(routeData);
       var filter = routeData.filter((item) => {
-        return item.RouteName.includes("河濱");
+        if (
+          item.RouteName.includes("河濱") ||
+          item.RouteName.includes("淡水河") ||
+          item.RouteName.includes("基隆河") ||
+          item.RouteName.includes("新店溪") ||
+          item.RouteName.includes("景美溪") ||
+          item.RouteName.includes("環社子島") ||
+          item.RouteName.includes("貴子坑溪") ||
+          item.RouteName.includes("關渡") ||
+          item.RouteName.includes("河濱自行車道")
+        ) {
+          return true;
+        }
+        return false;
       });
       console.log(filter);
 
